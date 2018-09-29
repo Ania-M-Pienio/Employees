05025d22a3470270cfb148d729baf3859323d053
unknown <apienio@myseneca.ca>
2018-09-29 13:07:23 -0400

    home and about

--- server.js
+++ server.js
@@ -4,9 +4,9 @@
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Ania M. Pienio  Student ID: 041780073 Date: September 14, 2018
+*  Name: Ania M. Pienio  Student ID: 041780073 Date: September 29, 2018
 *
-*  Online (Heroku) URL: https://sleepy-earth-12601.herokuapp.com/
+*  
 *
 ********************************************************************************/ 
 
@@ -15,11 +15,13 @@
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
+var path = require("path");
 
-// setup a 'route' to listen on the default url path
+
 app.get("/", (req, res) => {
-    res.send("Ania M. Pienio - 041780073");
+    console.log("Express http server listening on" + HTTP_PORT);
+    res.sendFile(path.join(__dirname, "/views/about.html"));
 });
 
-// setup http server to listen on HTTP_PORT
+
 app.listen(HTTP_PORT);
