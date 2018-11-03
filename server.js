f50a0fd51a3f8074f223bc493c6e10e0859be8bc
unknown <apienio@myseneca.ca>
2018-11-03 18:52:19 -0400

    Part 1 - Step 4 (with equal)

--- server.js
+++ server.js
@@ -29,6 +29,15 @@ app.engine('.hbs', exphbs({
             return '<li' +
             ((url == app.locals.activeRoute) ? ' class="active" ' : '') +
             '><a href="' + url + '">' + options.fn(this) + '</a></li>';
+        },
+        equal: function (lvalue, rvalue, options) {
+            if (arguments.length < 3)
+            throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+            return options.inverse(this);
+            } else {
+            return options.fn(this);
+            }
         }
     }
 }));
