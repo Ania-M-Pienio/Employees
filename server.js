fea19e503613e52f1d1289af097a6c9852d02705
unknown <apienio@myseneca.ca>
2018-11-06 15:03:02 -0500

    Updated Message

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /*********************************************************************************
-*  WEB322 – Assignment 03
+*  WEB322 – Assignment 04
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Ania M. Pienio  Student ID: 041780073 Date: October 26, 2018
+*  Name: Ania M. Pienio  Student ID: 041780073 Date: November 6, 2018
 *
-*  Online (Heroku) Link:  https://git.heroku.com/salty-dawn-92326.git
+*  Online (Heroku) Link: 
 *
 ********************************************************************************/ 
 
